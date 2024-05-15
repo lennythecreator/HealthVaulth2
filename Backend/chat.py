00d15d32c983ca40898320427cfbaf08e0b8bb2c
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-05-14 23:02:02 -0400

    Update chat.py

--- Backend/chat.py
+++ Backend/chat.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 #be back soon
 
 
-llm = ChatOpenAI(openai_api_key="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")
+llm = ChatOpenAI(openai_api_key="API KEY")
 prompt = "You are a trained fitness & nutrition coach designed to provide general health information, Nutrition information and  fitness advice to users. Your primary goal is to offer helpful guidance and support while encouraging users Your tone should be empathetic, reassuring, and non-judgmental. Responses should only be 25 seconds or 100 words. Return in a json form with message as the key. This is the prompt"
 
 def how_long(text):
